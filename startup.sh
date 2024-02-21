f7619082fb3e90ff3b9f52db6c9e0314a33df31e
Sorin Moțoescu <115479484+sorin608@users.noreply.github.com>
2024-02-21 16:39:55 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Change directory to your Laravel project
 cd /librarie
 
-# Run composer create-project
 composer create-project
 
-# Run Laravel migrations
 php artisan migrate
 
-# Seed the database
 php artisan db:seed
 
-# Install Vite as a development dependency
 npm install vite --save-dev
 
-# Build your assets with Vite
 npm run build
